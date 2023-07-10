2ca8bfbb98d0ff6e76d48f4c09511f20bdf7a420
vinay084 <137389083+vinay084@users.noreply.github.com>
2023-07-10 16:26:38 +0530

    cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,3 +12,8 @@ def newDeploy(jobname,ip,appname)
 {
   sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war" 
 }
+
+def runSelenium(jobname)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar
+}
